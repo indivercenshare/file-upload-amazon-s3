d031882c61ab2ce4f683bedafe936fff91588206
Indiver Kumar <ink@censhare.com>
2017-09-02 22:50:21 +0530

    MASTER

--- src/main/java/com/amazonaws/samples/Test.java
+++ src/main/java/com/amazonaws/samples/Test.java
@@ -6,44 +6,6 @@ public class Test {
 		System.out.println("Test");
 		System.out.println("Test2");
 		System.out.println("Test3");
-		System.out.println("Test4");
-		System.out.println("Test5");
-		System.out.println("Test-BUG102");
-		System.out.println("Test-FEATURE01");
-		System.out.println("Test-master");
-		System.out.println("Test-master2");
-		System.out.println("Test-feature01");
-		System.out.println("Test-feature02");
-		System.out.println("Test-feature03");
-		System.out.println("Test-feature04");
-		System.out.println("Test-feature05");
-		System.out.println("Test-featuream1");
-		System.out.println("Test-featuream2");
-		System.out.println("Test-featurelok1");
-		System.out.println("Test-featurelok2");
-		System.out.println("Test-feature06");
-		System.out.println("Test-feature07");
-		System.out.println("Test-feature08");
-		System.out.println("Test-feature09");
-		System.out.println("Test-featurelok");
-		System.out.println("Test-featurelok");
-		System.out.println("Test-featuream3");
-		System.out.println("Test-featuream4");
-		System.out.println("Test-featurelok");
-		System.out.println("Test-featurelok");
-		System.out.println("Test-amrinder");
-		System.out.println("Test-amrinder2");
-		System.out.println("Test-lokesh");
-		System.out.println("Test-lokesh2");
-		System.out.println("Test-amrinder3");
-		System.out.println("Test-amrinder4");
-		System.out.println("Test-INDIVER");
-		System.out.println("Test-AMRINDER5");
-		System.out.println("Test-INDIVER");
-		System.out.println("Test-LOKESH");
-		System.out.println("Test-INDIVER");
-		System.out.println("Test-AMRDINER");
-		System.out.println("Test-LOKESH");
-		System.out.println("Test-LOKESH2");
+		
 	}
 }
