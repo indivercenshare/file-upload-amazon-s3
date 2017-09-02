4bd62db68467287e19824e6378e043634ffa755a
Indiver Kumar <ink@censhare.com>
2017-09-02 23:14:06 +0530

    no message

--- src/main/java/com/amazonaws/samples/Test.java
+++ src/main/java/com/amazonaws/samples/Test.java
@@ -10,6 +10,9 @@ public class Test {
 		System.out.println("Test5");
 		System.out.println("Test6");
 		System.out.println("Test7");
+		System.out.println("Test8");
+		System.out.println("Test9");
+		System.out.println("Test10");
 		System.out.println("MASTER");
 		
 	}
